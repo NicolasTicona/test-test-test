336b8f5442ecff09352e8dfe81e3779836f21d10
Nicolas Ticona Valdivia <nikolasgamertv@gmail.com>
2022-01-17 20:56:56 -0500

    base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ZipcodeSearchComponent } from './zipcode-search/zipcode-search.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  declarations: [ AppComponent, ZipcodeSearchComponent, WeatherCardComponent, WeatherForecastComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/zipcode-search/zipcode-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zipcode-search',
+  templateUrl: './zipcode-search.component.html',
+  styleUrls: ['./zipcode-search.component.css']
+})
+export class ZipcodeSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
